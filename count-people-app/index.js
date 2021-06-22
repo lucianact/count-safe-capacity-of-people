3948ce6a7705e6e4aa0d2654b01c7f403b3b6e75
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-06-22 19:47:16 +0000

    Add increment() function

--- count-people-app/index.js
+++ count-people-app/index.js
@@ -1,1 +1,24 @@
 // javascript
+let addPersonButton = document.getElementById("increment-btn")
+let countPerson = document.getElementById("count-el")
+
+// with function and onclick html property 
+let people = 0
+const increment = () => {
+    people += 1;
+    countPerson.innerText = people;
+};
+
+// with event listener 
+// let count = 0
+// addPersonButton.addEventListener("click", () => {
+//     count += 1
+//     countPerson.innerHTML = count;
+// }); 
+
+// with jQuery:
+// let count = 0; 
+// $("#increment-btn").click( function() {
+//     count += 1;
+//     $("#count-el").text = count; 
+// });
